b23dd96d65bdcfb22394e35c613a9dbfe91afc6e
sami jaghouar <sami.jaghouar@jina.ai>
2022-10-11 11:23:05 +0200

    fix(whisper): handle tensor document

--- executors/whisper/executor.py
+++ executors/whisper/executor.py
@@ -12,7 +12,7 @@ class WhisperExecutor(Executor):
     def transcribe(self, docs: DocumentArray, **kwargs):
 
         for (i, doc_) in enumerate(docs):
-            model_output = self.model.transcribe(doc_.uri if not(doc_.tensor) else doc_.tensor)
+            model_output = self.model.transcribe(doc_.uri if doc_.tensor is None else doc_.tensor)
             doc_.text = model_output["text"]
             doc_.tags["segments"] = model_output["segments"]
             doc_.tags["language"] = model_output["language"]
