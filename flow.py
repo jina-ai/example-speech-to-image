2274355cc093fafe89f51510ba0480b96b02355d
Sami Jaghouar <sami.jaghouar@jina.ai>
2022-10-11 08:55:52 +0200

    feat: allow to use jcloud flow in ui

--- flow.py
+++ flow.py
@@ -4,5 +4,7 @@ from executors.stablediffusion import StableDiffusionExecutor
 import os
 
 hf_token = os.getenv('HF_TOKEN')
-with Flow(port=54322).add(uses=WhisperExecutor).add(uses=StableDiffusionExecutor, uses_with={'auth_token': hf_token}) as f:
+with Flow(port=54322).add(uses=WhisperExecutor).add(
+    uses=StableDiffusionExecutor, uses_with={'auth_token': hf_token}
+) as f:
     f.block()
--- ui.py
+++ ui.py
@@ -1,22 +1,34 @@
 import gradio as gr
 from docarray import Document
 from jina import Client
-client = Client(port=54322)
+import typer
 
-def speech_to_text(audio_file_uri):
-
-    docs = client.post(on='/', inputs=[Document(uri=audio_file_uri)],  parameters={'num_images':2})
-
-    for img in docs[0].matches:
-        img.load_uri_to_image_tensor()
+app = typer.Typer()
 
 
-    return [docs[0].matches[i].tensor for i in range(2)] + [docs[0].text]
-3
-gr.Interface(
-    fn=speech_to_text,
-    inputs=[
-        gr.Audio(source="microphone", type="filepath"),
-    ],
-    outputs=["image","image", "text"],
-).launch()
+@app.command()
+def main(host: str ='localhost:54322'):
+
+    client = Client(host = host)
+
+    def speech_to_text(audio_file_uri):
+
+        docs = client.post(
+            on='/', inputs=[Document(uri=audio_file_uri).load_uri_to_audio_tensor()], parameters={'num_images': 2}
+        )
+
+        for img in docs[0].matches:
+            img.load_uri_to_image_tensor()
+
+        return [docs[0].matches[i].tensor for i in range(2)] + [docs[0].text]
+
+    gr.Interface(
+        fn=speech_to_text,
+        inputs=[
+            gr.Audio(source='microphone', type='filepath'),
+        ],
+        outputs=['image', 'image', 'text'],
+    ).launch()
+
+if __name__ == '__main__':
+    app()
