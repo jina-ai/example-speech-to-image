093076afeb84746758ce1abf65ca6c6a7ee21d73
jina <alaeddine-13@live.fr>
2022-11-21 01:10:08 +0100

    document stable diffusion

--- executors/stablediffusion/executor.py
+++ executors/stablediffusion/executor.py
@@ -5,9 +5,19 @@ from diffusers import StableDiffusionPipeline
 
 
 class StableDiffusionExecutor(Executor):
-    '''Stable diffusion Executor to generate code image out of text'''
-
     def __init__(self, auth_token, **kwargs):
+        """
+        StableDiffusionExecutor uses the stable diffusion model (https://github.com/CompVis/stable-diffusion) to generate
+        images from text prompt.
+        The model is loaded using the diffusers library under the pipeline 'CompVis/stable-diffusion-v1-4'.
+        It is required to provide a HF authentication token with the user accepting the ToS for using the model.
+        The StableDiffusionExecutor expects documents with text prompt stored in `Document.text`, will generate up to
+        `num_images` images represented as Document objects. The Document objects contain the image data stored in the
+        `Document.uri` field and are added to the query `Document.matches` field.
+        The `num_images` parameter can be specified as a key-value pair in `parameters` of the request.
+
+        :param auth_token: HF auth token
+        """
         super().__init__()
         self.diffusion = StableDiffusionPipeline.from_pretrained(
             'CompVis/stable-diffusion-v1-4',
@@ -18,6 +28,13 @@ class StableDiffusionExecutor(Executor):
 
     @requests(on='/')
     def generate(self, docs: DocumentArray, parameters: dict, **kwargs):
+        """
+        Generate images from text using the StableDiffusion model.
+        This endpoint expects  documents with text prompt stored in `Document.text`, will generate up to
+        `num_images` images represented as Document objects. The Document objects contain the image data stored in the
+        `Document.uri` field and are added to the query `Document.matches` field.
+        The `num_images` parameter can be specified as a key-value pair in `parameters` of the request.
+        """
 
         num_images = parameters.get('num_images', 1)
 
